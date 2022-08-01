fcc448c2a2410a9e54f66ce75f3007037941790a
itsjohncs <johnsullivan.pem@gmail.com>
2022-08-01 15:53:27 -0700

    Respect multiplier in register view.

--- src/tool/views/register.py
+++ src/tool/views/register.py
@@ -10,6 +10,12 @@ def floor_delta(delta: datetime.timedelta) -> datetime.timedelta:
     return datetime.timedelta(seconds=math.floor(delta.total_seconds()))
 
 
+def scale(
+    duration: datetime.timedelta, multiplier: float
+) -> datetime.timedelta:
+    return datetime.timedelta(seconds=duration.total_seconds() * multiplier)
+
+
 def render(logs: logfile.LogFile):
     """Prints a human-readable summary of `logs`."""
     total_time = datetime.timedelta(0)
@@ -27,6 +33,9 @@ def render(logs: logfile.LogFile):
             duration = floor_delta(datetime.datetime.now() - check_in.timestamp)
             status = "*"
 
+        multiplier = float(entry.metadata.get("Multiplier", 1.0))
+        scaled_duration = floor_delta(scale(duration, multiplier))
+
         if day_total is not None and check_in.timestamp.date() != day_total[0]:
             pretty_date = day_total[0].strftime("%h %d")
             print(f"{pretty_date} SUBTOTAL\t\t({day_total[1]})")
@@ -35,15 +44,18 @@ def render(logs: logfile.LogFile):
         if day_total is None:
             day_total = (check_in.timestamp.date(), datetime.timedelta())
 
-        day_total = (day_total[0], day_total[1] + duration)
-        total_time += duration
+        day_total = (day_total[0], day_total[1] + scaled_duration)
+        total_time += scaled_duration
 
         pretty_timestamp = datetime.datetime.strftime(
             check_in.timestamp, "%h %d @ %I:%M %p"
         )
 
+        pretty_multiplier = "" if multiplier == 1.0 else f" / {multiplier:1.1}"
+
         print(
-            f"{pretty_timestamp}\t{duration}{status}\t{entry.account}: {entry.task}"
+            f"{pretty_timestamp}\t{scaled_duration}{pretty_multiplier}{status}"
+            f"\t{entry.account}: {entry.task}"
         )
 
     if day_total is not None:
