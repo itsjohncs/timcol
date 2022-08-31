563ebeaa56255b3a6318a99c6d03373f92907b90
itsjohncs <johnsullivan.pem@gmail.com>
2022-08-31 14:56:51 -0700

    Fix rounding issue when pretty-printing durations
    We'd round up the hour if the minutes were >30, and same for the
    minutes if the seconds were >30.

--- src/tool/views/_shared.py
+++ src/tool/views/_shared.py
@@ -3,4 +3,4 @@ import datetime
 
 def pretty_duration(duration: datetime.timedelta) -> str:
     sec = duration.total_seconds()
-    return f"{sec / 60**2:01.0f}:{sec / 60 % 60:02.0f}:{sec % 60:02.0f}"
+    return f"{sec // 60**2:01.0f}:{sec // 60 % 60:02.0f}:{sec % 60:02.0f}"
